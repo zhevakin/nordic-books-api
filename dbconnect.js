cf9d32a3c52e9a86d03b031ba87d480fc9aa21d3
Ivan Zhevakin <ixvakin@gmail.com>
2022-05-03 18:46:14 +0300

    make it better

--- dbconnect.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.Promise = require('bluebird')
-const url = process.env.MONGODB_URI
-const connect = mongoose.connect(url)
-module.exports = connect
---
+++ index.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+
+const { MongoClient, ObjectId } = require('mongodb')
+const express = require('express')
+const bodyParser= require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use(cors({ origin: '*' }))
+
+MongoClient.connect(process.env.MONGODB_URI, { useUnifiedTopology: true })
+  .then(client => {
+    const db = client.db('books')
+    const booksCollection = db.collection('books')
+
+    app.get('/books', function (req, res) {
+      booksCollection.find().toArray()
+        .then(results => {
+          res.json(results)
+        })
+        .catch(error => console.error(error))
+    })
+
+    app.post('/books', (req, res) => {
+      booksCollection.insertOne({
+        author: req.body.author,
+        title: req.body.title,
+      }).then(result => {
+        res.json(result)
+      })
+    })
+
+    app.delete('/books/:id', (req, res) => {
+      booksCollection.deleteOne({ _id: new ObjectId(req.params.id) })
+        .then(result => {
+          res.json(result)
+        })
+    })
+
+    app.put('/books/:id', (req, res) => {
+      booksCollection.updateOne({ _id: new ObjectId(req.params.id) }, {$set: {
+          author: req.body.author,
+          title: req.body.title,
+        }})
+        .then(result => {
+          res.json(result)
+        })
+    })
+
+    app.listen(3000, function() {
+      console.log('listening on 3000')
+    })
+
+  })
