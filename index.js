0cdb23d4aa37befa43955d223fd87a4d538a988d
Ivan Zhevakin <ixvakin@gmail.com>
2022-05-03 20:58:29 +0300

    user id

--- index.js
+++ index.js
@@ -18,23 +18,38 @@ MongoClient.connect(process.env.MONGODB_URI, { useUnifiedTopology: true })
     const db = client.db('books')
     const booksCollection = db.collection('books')
 
+    // Список книг
     app.get('/books', function (req, res) {
-      booksCollection.find().toArray()
+      const userId = req.headers['user-id']
+      const filter = userId ? { userId } : {}
+      booksCollection.find(filter).toArray()
         .then(results => {
           res.json(results)
         })
         .catch(error => console.error(error))
     })
 
+    app.get('/books/:id', function (req, res) {
+      booksCollection.findOne({ _id: new ObjectId(req.params.id) })
+        .then(results => {
+          res.json(results)
+        })
+        .catch(error => console.error(error))
+    })
+
+    // Добавление книги
     app.post('/books', (req, res) => {
+      const userId = req.headers['user-id']
       booksCollection.insertOne({
         author: req.body.author,
         title: req.body.title,
+        ...(userId ? { userId } : {})
       }).then(result => {
         res.json(result)
       })
     })
 
+    // Удаление книги
     app.delete('/books/:id', (req, res) => {
       booksCollection.deleteOne({ _id: new ObjectId(req.params.id) })
         .then(result => {
@@ -42,6 +57,7 @@ MongoClient.connect(process.env.MONGODB_URI, { useUnifiedTopology: true })
         })
     })
 
+    // Обновление книги
     app.put('/books/:id', (req, res) => {
       booksCollection.updateOne({ _id: new ObjectId(req.params.id) }, {$set: {
           author: req.body.author,
